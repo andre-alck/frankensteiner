5edf60b64d69a73ce78b7f20d5c767fa92e4cfca
andre-alck <andrealck1@gmail.com>
2024-03-20 23:11:47 -0300

    feat: add EmptyExtensionException; init list of supported extensions

--- app/src/main/java/app/StitchService.java
+++ app/src/main/java/app/StitchService.java
@@ -1,70 +1,65 @@
 package app;
 
 public abstract class StitchService {
-    protected String extension;
-    protected String path;
+	private String extension;
+	private String path;
 
-    private final static String WHITESPACE = "";
-    private final static String DOT = ".";
-    private static final int INDEX_NOT_FOUND = -1;
+	private final static String DOT = ".";
+	private static final int INDEX_NOT_FOUND = -1;
 
-    public StitchService(String extension, String path) {
-        this.setExtension(extension);
-        this.setPath(path);
+	public StitchService(String extension, String path) {
+		this.setExtension(extension);
+		this.setPath(path);
 
-        this.checkIfExtensionIsValid();
-    }
+		this.checkIfExtensionIsValid();
+	}
 
-    private void checkIfExtensionIsValid() {
-        this.removeDotIfNeeded();
+	protected void checkIfExtensionIsValid() {
+		this.removeDotIfNeeded();
 
-        if(this.isContentEmpty(this.getExtension())) {
-            throw new RuntimeException("fixme - add custom exception for dealing with missing extension");
-        }
+		if (StringUtils.isContentEmpty(this.getExtension())) {
+			throw new EmptyExtensionException();
+		}
 
-        if(this.isExtensionUnsupported()) {
-            throw new RuntimeException("fixme - add custom exception for unsupported extension");
-        }
-    }
+		if (this.isExtensionUnsupported()) {
+			throw new RuntimeException("fixme - add custom exception for unsupported extension");
+		}
+	}
 
-    private void removeDotIfNeeded() {
-        int indexOfDot = this.getExtension().indexOf(StitchService.DOT);
-        if(indexOfDot == StitchService.INDEX_NOT_FOUND) {
-            return;
-        }
+	protected void removeDotIfNeeded() {
+		int indexOfDot = this.getExtension().indexOf(StitchService.DOT);
+		if (indexOfDot == StitchService.INDEX_NOT_FOUND) {
+			return;
+		}
 
-        StringBuilder extension = new StringBuilder();
-        extension.append(this.getExtension());
-        extension.deleteCharAt(indexOfDot);
-        this.setExtension(extension.toString());
-    }
+		StringBuilder extension = new StringBuilder();
+		extension.append(this.getExtension());
+		extension.deleteCharAt(indexOfDot);
+		this.setExtension(extension.toString());
+	}
 
-    private boolean isContentEmpty(String content) {
-        return content == null || content.trim() == StitchService.WHITESPACE;
-    }
+	protected boolean isExtensionUnsupported() {
+//        throw new RuntimeException("missing impl!");
+		return false;
+	}
 
-    private boolean isExtensionUnsupported() {
-        // use Extensions enum
-        throw new RuntimeException("missing impl!");
-    }
+	protected String getExtension() {
+		return extension;
+	}
 
-    protected String getExtension() {
-        return extension;
-    }
+	protected void setExtension(StringBuilder extension) {
+		this.setExtension(extension.toString());
+	}
 
-    protected void setExtension(StringBuilder extension) {
-        this.setExtension(extension.toString());
-    }
+	protected void setExtension(String extension) {
+		this.extension = extension;
+	}
 
-    protected void setExtension(String extension) {
-        this.extension = extension;
-    }
+	protected String getPath() {
+		return path;
+	}
 
-    protected String getPath() {
-        return path;
-    }
-
-    protected void setPath(String path) {
-        this.path = path;
-    }
+	protected void setPath(String path) {
+		this.path = path;
+	}
 }
---
+++ app/src/main/java/app/SupportedExtensions.java
@@ -0,0 +1,5 @@
+package app;
+
+public enum SupportedExtensions {
+	SQL, TXT
+}
