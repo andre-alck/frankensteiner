07cb8de82ed7192d74435422cedfd67fff393bfa
andre-alck <andrealck1@gmail.com>
2024-03-25 22:15:03 -0300

    test: PathService#isFileCountWithExtensionMoreThanOne

--- app/src/test/java/app/PathServiceTest.java
+++ app/src/test/java/app/PathServiceTest.java
@@ -4,20 +4,35 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class PathServiceTest {
 
 	@Test
 	void givenSearchForPath_whenPathExists_thenReturnTrue() {
 		final String existingPath = "/home/linkedrh/Desktop";
-
 		assertTrue(PathService.isPathExistent(existingPath));
 	}
 
 	@Test
-	void givenSearchForPath_whenPathDoesntExists_thenReturnFalse() {
+	void givenSearchForPath_whenPathDoesNotExist_thenReturnFalse() {
 		final String nonExistingPath = "testfolder";
-
 		assertFalse(PathService.isPathExistent(nonExistingPath));
+	}
+
+	@Test
+	void givenSearchForExtensionInPath_whenThereAreAtLeastTwoFilesWithThatExtension_thenReturnTrue() {
+		final String extension = "sql";
+		assertTrue(PathService.isFileCountWithExtensionMoreThanOne(extension,
+				"/home/linkedrh/Desktop/PathServiceTest/givenSearchForExtensionInPath_whenThereAreAtLeastTwoFilesWithThatExtension_thenReturnTrue"));
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings = { "txt", "nonexistent" })
+	void givenSearchForExtensionInPath_whenThereAreNotAtLeastTwoFilesWithThatExtension_thenReturnFalse(
+			String extension) {
+		assertFalse(PathService.isFileCountWithExtensionMoreThanOne(extension,
+				"/home/linkedrh/Desktop/PathServiceTest/givenSearchForExtensionInPath_whenThereAreNotAtLeastTwoFilesWithThatExtension_thenReturnFalse/"));
 	}
 }
