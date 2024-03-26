0b6ac9abe42ee047739e6f130d4c5a2a6b220e56
andre-alck <andrealck1@gmail.com>
2024-03-25 22:14:12 -0300

    feat: file count based on extension

--- app/src/main/java/app/PathService.java
+++ app/src/main/java/app/PathService.java
@@ -1,5 +1,7 @@
 package app;
 
+import java.io.File;
+import java.io.FileFilter;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -9,6 +11,30 @@ public class PathService {
 	public static boolean isPathExistent(String path) {
 		Path directoryPath = Paths.get(path);
 		return Files.isDirectory(directoryPath);
+	}
+
+	public static boolean isFileCountWithExtensionMoreThanOne(String extension, String path) {
+		File folder = new File(path);
+		FileFilter fileFilter = PathService.getFileFilter(extension);
+		File[] files = folder.listFiles(fileFilter);
+
+		int fileCount = 0;
+		for (int i = 0; i < files.length; i++) {
+			if (files[i].getName().endsWith(extension)) {
+				fileCount++;
+			}
+		}
+
+		return fileCount > 1;
+	}
+
+	private static FileFilter getFileFilter(String extension) {
+		return new FileFilter() {
+			@Override
+			public boolean accept(File pathname) {
+				return pathname.getName().endsWith(extension);
+			}
+		};
 	}
 
 }
