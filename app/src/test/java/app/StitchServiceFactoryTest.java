fde23b96b8f8c6fd37d5c91e159f791b49d75be7
andre-alck <andrealck1@gmail.com>
2024-03-31 18:32:44 -0300

    test: different extension implementations based on input

--- app/src/test/java/app/StitchServiceFactoryTest.java
+++ app/src/test/java/app/StitchServiceFactoryTest.java
@@ -2,16 +2,15 @@ package app;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-class StitchServiceFactoryTest {
-    
-    @ParameterizedTest
-    @ValueSource(strings = {"SQL", "TXT"})
-    void givenValidInput_whenCallingForInstanceImplementation_shouldReturnStitchServiceImplementation(String validInput) {
-        StitchService service = StitchServiceFactory.getImplementation(validInput);
-        assertNotNull(service);
-    }
+public class StitchServiceFactoryTest {
+
+	@ParameterizedTest
+	@ValueSource(strings = { "SQL", "TXT" })
+	public void givenValidInput_whenCallingForInstanceImplementation_shouldReturnStitchServiceImplementation(String validInput) {
+		StitchService service = StitchServiceFactory.getImplementation(validInput);
+		assertNotNull(service);
+	}
 }
