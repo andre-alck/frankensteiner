19ce736ad858ed7db9892a3b3becdf7f9f702f0a
andre-alck <andrealck1@gmail.com>
2024-04-07 21:16:31 -0300

    test: order when calling stitching method

--- app/src/test/java/app/StitchServiceTest.java
+++ app/src/test/java/app/StitchServiceTest.java
@@ -1,24 +1,46 @@
 package app;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doCallRealMethod;
+import static org.mockito.Mockito.inOrder;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.mockito.Answers;
+import org.mockito.InOrder;
 
 class StitchServiceTest {
 	private static final int AMOUNT_OF_TXT_FILES = 5;
 
 	@Test
-	void givenFolderContainingFiveFiles_whenEachFileIsRead_shouldStoreItsData() {
+	void givenFolderContainingFiveFiles_whenEachFileIsRead_thenShouldStoreItInList() {
 		StitchService stitchService = mock(StitchService.class, Answers.CALLS_REAL_METHODS);
 		when(stitchService.getExtension()).thenReturn(Extensions.TXT);
 
-		List<FileData> singleFile = stitchService.readFiles("/home/linkedrh/Desktop/StitchServiceTest/givenFolderContainingFiveFiles_whenEachFileIsRead_shouldStoreItsData");
-		assertEquals(StitchServiceTest.AMOUNT_OF_TXT_FILES, singleFile.size());
+		List<FileData> filesData = stitchService.readFiles("/home/linkedrh/Desktop/StitchServiceTest/givenFolderContainingFiveFiles_whenEachFileIsRead_shouldStoreItsData");
+		assertEquals(StitchServiceTest.AMOUNT_OF_TXT_FILES, filesData.size());
+	}
+
+	@Test
+	void givenStichingMethod_whenVerifyingItsLogic_shouldReadThenWrite() {
+		StitchService stitchService = mock(StitchService.class);
+		doCallRealMethod().when(stitchService).stitch(anyString(), any(SortingWay.class));
+
+		SortingWay sortingWay = new AlphabeticalSorting();
+		List<FileData> filesData = new ArrayList<>();
+		stitchService.stitch("", sortingWay);
+
+		InOrder inOrder = inOrder(stitchService);
+		inOrder.verify(stitchService, times(1)).readFiles(anyString());
+		inOrder.verify(stitchService, times(1)).writeFile(filesData, sortingWay);
 	}
 
 }
