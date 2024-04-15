cbec2c81c15dcf74ae9acc6231bb9ded68d1a200
andre-alck <andrealck1@gmail.com>
2024-04-14 23:12:54 -0300

    docs: todo comment just to organize my ideas

--- app/src/main/java/app/StitchServiceSQLImpl.java
+++ app/src/main/java/app/StitchServiceSQLImpl.java
@@ -11,6 +11,10 @@ public class StitchServiceSQLImpl extends StitchService {
 		return Extensions.SQL;
 	}
 
+	// TODO: since it's a sql file, there should be a comment indicating the task
+	// number etc.
+	// also, i'd like to find a way to sort the files before writing to the stitched file.
+	// i won't use the StitchService#getConcatenatedStringThroughFileData, it's too generic, can't do much.
 	@Override
 	protected void writeFile(List<FileData> filesData, SortingWay sortingWay) {
 		try (FileWriter stitchedFileWriter = new FileWriter(this.getResultFileName())) {
