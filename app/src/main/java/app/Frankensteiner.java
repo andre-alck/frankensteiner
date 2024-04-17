7c772b42561f970049da4d7e7bd195aebca5fd60
andre-alck <andrealck1@gmail.com>
2024-04-16 23:09:57 -0300

    feat: stitch method; need to work on varargs to get extension, path and sortingway dinamically

--- app/src/main/java/app/Frankensteiner.java
+++ app/src/main/java/app/Frankensteiner.java
@@ -4,10 +4,12 @@ public class Frankensteiner {
 	public static void main(String[] args) {
 		String extension = "";
 		String path = "";
+		SortingWay sortingWay = null;
 
 		PathService.checkIfPathIsValid(extension, path);
 		ExtensionService.checkIfExtensionIsValid(extension);
 
-		StitchServiceFactory.getImplementation(extension);
+		StitchService service = StitchServiceFactory.getImplementation(extension);
+		service.stitch(path, sortingWay);
 	}
 }
